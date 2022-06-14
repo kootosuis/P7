841d1468e55735445b08e1d0f3fa696c8b647676
kootosuis <poursaintgervais@gmail.com>
2022-06-14 22:48:51 +0200

    à vérifier avec JC

--- frontend/src/views/OnePostedShare.vue
+++ frontend/src/views/OnePostedShare.vue
@@ -1,28 +1,37 @@
 <template>
-<div>
-		<header class="header">
-			<NavBar/>
-		</header>
-		<div class="main">
-			<PostAShare/>
-                              <ShareAlone/>
-		</div>
-		
-	</div>
+    <div>
+        <header class="header">
+            <NavBar />
+        </header>
+        <div class="main">
+            <PostAShare />
+            <ShareAlone />
+            <!-- <ShareAlone :key="componentKey" /> -->
+        </div>
+    </div>
 </template>
 
-
 <script>
-          import NavBar from "@/components/NavBar.vue"
-          import PostAShare from "@/components/PostAShare.vue"
-          import ShareAlone from "@/components/ShareAlone.vue"
-         
-	export default {
-		name: "OnePostedShare",
-		components: {
-                              NavBar,
-                              PostAShare,
-                              ShareAlone
-		}
-	};
+    import NavBar from "@/components/NavBar.vue";
+    import PostAShare from "@/components/PostAShare.vue";
+    import ShareAlone from "@/components/ShareAlone.vue";
+
+    export default {
+        name: "OnePostedShare",
+        components: {
+            NavBar,
+            PostAShare,
+            ShareAlone,
+        },
+        //         data() {
+        //             return {
+        //                 componentKey: 0,
+        //             };
+        //         },
+        //         methods: {
+        //             forceRerender() {
+        //                 this.componentKey += 1;
+        //             },
+        //         },
+    };
 </script>
